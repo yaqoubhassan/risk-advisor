e809a853e07344443de0143498fba61bb27e0ea6
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-03-11 00:38:36 +0000

    update button component

--- resources/js/components/common/IButton.vue
+++ resources/js/components/common/IButton.vue
@@ -1,26 +1,35 @@
+s
 <script setup>
 const props = defineProps({
-  /**
-   * @type {'button'|'submit'|'reset'}
-   */
-  type: {
-    type: String,
-    default: "button",
-    validator(value) {
-      return ["button", "submit", "reset"].includes(value);
+    /**
+     * @type {'button'|'submit'|'reset'}
+     */
+    type: {
+        type: String,
+        default: "button",
+        validator(value) {
+            return ["button", "submit", "reset"].includes(value);
+        },
     },
-  },
 });
 </script>
 
 <template>
-  <button :type="props.type">
-    <slot></slot>
-  </button>
+    <button :type="props.type">
+        <slot></slot>
+    </button>
 </template>
 
 <style scoped>
 button {
-  @apply bg-blue-500 text-white py-3 px-4 rounded-xl w-full hover:bg-blue-600;
+    @apply text-white py-3 px-4 rounded-xl w-full;
+
+    &:disabled {
+        @apply bg-gray-300 cursor-not-allowed;
+    }
+
+    &:not(:disabled) {
+        @apply cursor-pointer bg-blue-500 hover:bg-blue-600;
+    }
 }
 </style>
