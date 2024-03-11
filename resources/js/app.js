f48993ed9aa6e3602005556d45e4cb9f06655c7c
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-03-11 00:40:59 +0000

    refactor app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,26 @@
+import {createApp} from "vue";
+import {createRouter, createWebHistory} from "vue-router";
+import "../css/app.css";
+import App from "./App.vue";
+import Home from "./routes/index.vue";
+import PersonalInformation from "./routes/personal-information.vue";
+import AddressInformation from "./routes/address-information.vue";
+import Success from "./routes/success.vue";
 import "./bootstrap";
 
-import { createApp } from "vue";
-import App from "./App.vue";
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: "/", component: Home},
+        {path: "/personal-information", component: PersonalInformation},
+        {path: "/address-information", component: AddressInformation},
+        {path: "/success", component: Success},
+    ],
+});
 
 const app = createApp(App);
+
+app.use(router);
 
 app.mount("#app");
 
